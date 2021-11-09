578230a7fa00eda14685ba155cc6509971551951
Dmitriy Bazhenov <63171863+thomas-marth@users.noreply.github.com>
2021-11-09 22:55:42 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+  return fetch(`https://restcountries.com/rest/v2/name/${searchQuery}`)
     .then(response => response.json())
     .then(data => data)
     .catch(error => console.log(error));
